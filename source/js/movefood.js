cc4f46df29511d46317806e6d9954da71b827ae1
RC Johnson <rc.johnson@gmail.com>
2011-06-05 16:30:19 -0500

    add items

--- source/js/movefood.js
+++ source/js/movefood.js
@@ -294,7 +294,7 @@ moveFood.addItem = function() {
 moveFood.requireAuthentication = function(url) {
     user = moveFood.isUserLoggedIn(function (user) {
         if (moveFood.isLoggedIn(user) && url != undefined) {
-            window.location = url
+            window.location = url;
         } else  {
             moveFood.showLogin();
         }

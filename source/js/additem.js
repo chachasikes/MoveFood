a48980482ea30148fd9813f550136f84b44073f4
Chach Sikes <chachasikes@gmail.com>
2011-06-04 21:15:53 -0700

    Added create item function (which is not yet working.)

--- source/js/additem.js
+++ source/js/additem.js
@@ -1,26 +1,3 @@
 $(document).ready(function(){
-
-  $('form#add-item-block-form .form-submit').click(function(){
-    var item = {};
-    item.name = $('form#add-item-block-form #food_name').val();
-    item.description = $('form#add-item-block-form #food_description').val();
-    item.quantity = $('form#add-item-block-form #food_quantity').val();
-    item.units = $('form#add-item-block-form #food_units').val();
-    item.perishable = $('form#add-item-block-form #food_perishable:checked').val();
-    item.expiration = $('form#add-item-block-form #food_expiration').val();
-    item.location_description = $('form#add-item-block-form #food_location_description:checked').val();
-    item.default_location = $('form#add-item-block-form #food_default_location').val();
-    item.notes = $('form#add-item-block-form #food_notes').val();
-    item.contact_detail = $('form#add-item-block-form #food_contact_detail').val();
-    item.default_contact = $('form#add-item-block-form #food_default_contact:checked').val();
-
-/*
-    $.post('ajax/test.html', function(data) {
-    
-    }
-*/
-    
-    return false;
-  });
-
+  $('form#add-item-block-form .form-submit').click(moveFood.addItemSubmit);
 });
--- source/js/showitem.js
+++ source/js/showitem.js
@@ -1,43 +1,10 @@
 $(document).ready(function(){
-
-
-    var item = {};
-    item.name = "name";
-    item.description = "description";
-    item.quantity = "quantity";
-    item.perishable = "perishable";
-    item.expiration = "expiration";
-    item.location = "location";
-    item.notes = "notes";
-    item.contact = "contact";
-
-            
-    $('div#content div#food_name').html(item.name);
-    $('div#content div#food_description').html(item.description);
-    $('div#content div#food_quantity').html(item.quantity);
-    $('div#content div#food_perishable').html(item.perishable);
-    $('div#content div#food_expiration').html(item.expiration);
-    $('div#content div#food_location').html(item.location);
-    $('div#content div#food_notes').html(item.notes);
-    $('div#content div#food_contact').html(item.contact);
-
-    
-    
-
-/*
-    item.description = $('form#add-item-block-form #food_description').val();
-    item.quantity = $('form#add-item-block-form #food_quantity').val();
-    item.units = $('form#add-item-block-form #food_units').val();
-    item.perishable = $('form#add-item-block-form #food_perishable:checked').val();
-    item.expiration = $('form#add-item-block-form #food_expiration').val();
-    item.location_description = $('form#add-item-block-form #food_location_description:checked').val();
-    item.default_location = $('form#add-item-block-form #food_default_location').val();
-    item.notes = $('form#add-item-block-form #food_notes').val();
-    item.contact_detail = $('form#add-item-block-form #food_contact_detail').val();
-    item.default_contact = $('form#add-item-block-form #food_default_contact:checked').val();
-*/
-
-    
-
-
+    $.ajax({
+      url: "http://www.movefood.krangarajan.com/movefood/index.php/list_items",
+      dataType: 'json',         
+      method: "post",
+      success: moveFood.showItem,
+      error: moveFood.error,
+    });
 });
+

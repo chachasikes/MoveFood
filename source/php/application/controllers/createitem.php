505ac37b1f5da2e2ea64d14d2f6afaeebeecfa6d
karthikrangarajan <karthik.rulz.forever@gmail.com>
2011-06-05 15:42:26 -0700

    Unscrewing, hopefully.

--- source/php/application/controllers/createitem.php
+++ source/php/application/controllers/createitem.php
@@ -11,13 +11,11 @@ class Createitem extends CI_Controller {
 		$food_name = $this->input->post('food_name');
 		$description = $this->input->post('description');
 		$quantity = $this->input->post('quantity');
-		$units = $this->input->post('units');
 		$perishable = $this->input->post('perishable');
 		$expiration = $this->input->post('expiration');
 		$location_description = $this->input->post('location_description');
-		$location = $this->input->post('default_location');
 		$notes = $this->input->post('notes');
-		$contact = $this->input->post('default_contact');
-		$this->claim_model->insert_item($food_name,$description,$quantity,$units,$perishable,$expiration,$location_description,$location,$notes,$contact);
+    $contact = $this->input->post('contact_detail');
+		$this->claim_model->insert_item($food_name,$description,$quantity,$perishable,$expiration,$location_description,$notes);
 	}
 }
